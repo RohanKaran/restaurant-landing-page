8936d3355b9508c8f376df56d92afda9b296664b
Rohan Karan <rohankaran001@gmail.com>
2023-04-16 19:45:00 +0530

    init

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import TrackVisibility from 'react-on-screen';
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
+import 'animate.css';
 import './Header.css';
 
 const Header = () => (
@@ -9,12 +11,30 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavour" />
       <h1 className="app__header-h1">The Key To Fine Dining</h1>
-      <p className="p__opensans" style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
-      <button type="button" className="custom__button">Explore Menu</button>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>
+        Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat
+        morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet
+        tellus{' '}
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
     </div>
 
     <div className="app__wrapper_img">
-      <img src={images.welcome} alt="header_img" />
+      <TrackVisibility partialVisibility>
+        {({ isVisible }) => (
+          <img
+            src={images.welcome}
+            alt="header_img"
+            className={
+              isVisible
+                ? 'animate__animated animate__slideInRight animate__slow'
+                : ''
+            }
+          />
+        )}
+      </TrackVisibility>
     </div>
   </div>
 );
